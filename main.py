a71f98cb96fbf014880c1d4297ea2f470ea41465
Kayra <kss367@drexel.edu>
2026-02-20 15:32:55 -0500

    Added a text analysis page (/analyze) and functions: clean_text, analyze_text

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os
+import re
+import string
 from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 app.secret_key = "secret-key"  
 acts = []
+
+# load in white nights text
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(base_dir, 'data', 'white-nights.txt')
+with open(file_path, "r", encoding="utf-8") as file: 
+    text = file.read()
 
 @app.route('/')
 def index():
@@ -15,18 +23,21 @@ def login():
     session["username"] = username
     return redirect(url_for("welcome"))
 
-@app.route('/login', methods=['POST'])
-def register():
-    return redirect(url_for("register"))
+''' TODO: change the route that this starts from. It can't also start from /login''' 
 
 @app.route('/register', methods=['POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/signup', methods=['POST'])
 def signup():
     username = request.form.get("username")
     session["username"] = username
     password1 = request.form.get("password1")
     password2 = request.form.get("password2")
     if password1 == password2: 
-        return render_template("index.html")
+        session["username"] = username
+        return redirect(url_for("welcome"))
     else: 
         return "<h1>Passwords do not match. Please try again.</h1>"
     
@@ -34,19 +45,38 @@ def signup():
 def welcome():
     username = session.get("username", None)
 
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(base_dir, 'data', 'white-nights.txt')
-
-    with open(file_path, "r", encoding="utf-8") as file: 
-        text = file.read()
-
     return render_template("welcome.html", username=username.title(), text=text)
 
+@app.route('/analyze')
+def analyze(): 
+    results = analyze_text(text)
+    return render_template("analyze.html", **results)
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"[^\w\s]", "", text)
+    text = text.replace("\n", " ")
+    text = re.sub(r"\s+", " ", text).strip()
+    return text
+
+def analyze_text(text):
+    cleaned_text = clean_text(text)
+    words = cleaned_text.split()
+    word_count = len(words)
+    unique_words = set(words)
+    unique_word_count = len(unique_words)
+    return {
+        "word_count": len(words),
+        "unique_word_count": len(set(words))
+    }
+
+'''
 @app.route('/act')
 def addact():
     actname = session.get("act", None)
     acts.append(actname)
     return render_template('act.html')
+'''
 
 '''
 @app.route('/populateacts')
