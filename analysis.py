d170c6050683b85b1ea9ee518eddf9388454aa49
Kayra <kss367@drexel.edu>
2026-02-25 10:14:30 -0500

    put in bare-bones skeleton of functions in analysis.py

---
+++ analysis.py
@@ -0,0 +1,15 @@
+def read_file(filename):
+    # read a file
+    return ""
+
+def count_words(text):
+    # count the number of words in a text
+    return ""
+
+def word_historgram(text):
+    # create a histogram of word frequencies
+    return ""
+
+def most_common_words(text):
+    # find the most common words in a text
+    return ""
